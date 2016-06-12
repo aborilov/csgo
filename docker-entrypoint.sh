ec69b1b7946c96ad841aef687875d911d0a63be6
Aborilov Pavel <aborilov@gmail.com>
2016-06-12 15:53:03 +0300

    add nameserver

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /opt/steamcmd/
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 ping -c 10 www.ru
 if [ ! -f /opt/steamcmd/csgo/finish ]; then
     ./steamcmd.sh +login anonymous +force_install_dir /opt/steamcmd/csgo +app_update 740 validate +quit
